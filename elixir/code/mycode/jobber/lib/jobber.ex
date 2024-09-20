f97b8cb00e46cd66df80cadbc90f63a9f7ef7f1b
SYCT <sict@bap.jp>
2024-09-20 13:49:05 +0700

    supervisor

--- elixir/code/mycode/jobber/lib/jobber.ex
+++ elixir/code/mycode/jobber/lib/jobber.ex
@@ -6,6 +6,13 @@ defmodule Jobber do
     # DynamicSupervisor.start_child(JobRunner, {Job, args})
   end
 
+  def running_imports() do
+    match_all = {:"$1", :"$2", :"$3"}
+    guards = [{:"==", :"$3", "import"}]
+    map_result = [%{id: :"$1", pid: :"$2", type: :"$3"}]
+    Registry.select(Jobber.JobRegistry, [{match_all, guards, map_result}])
+  end
+
   @moduledoc """
   Documentation for `Jobber`.
   """
