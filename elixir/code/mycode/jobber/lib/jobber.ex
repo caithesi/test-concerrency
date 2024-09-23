ebe2c586f85f251a4b17d24ee1848e0d87c9b35f
SYCT <sict@bap.jp>
2024-09-23 16:03:17 +0700

    limit number of job in jobber

--- elixir/code/mycode/jobber/lib/jobber.ex
+++ elixir/code/mycode/jobber/lib/jobber.ex
@@ -2,13 +2,16 @@ defmodule Jobber do
   alias Jobber.{JobRunner, JobSupervisor, Job}
 
   def start_job(args) do
-    DynamicSupervisor.start_child(JobRunner, {JobSupervisor, args})
-    # DynamicSupervisor.start_child(JobRunner, {Job, args})
+    if Enum.count(running_imports()) >= 5 do
+      {:error, :import_quota_reached}
+    else
+      DynamicSupervisor.start_child(JobRunner, {JobSupervisor, args})
+    end
   end
 
   def running_imports() do
     match_all = {:"$1", :"$2", :"$3"}
-    guards = [{:"==", :"$3", "import"}]
+    guards = [{:==, :"$3", "import"}]
     map_result = [%{id: :"$1", pid: :"$2", type: :"$3"}]
     Registry.select(Jobber.JobRegistry, [{match_all, guards, map_result}])
   end
