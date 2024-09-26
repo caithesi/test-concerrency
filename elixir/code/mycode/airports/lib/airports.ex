616803735d51257744c40533e644888f659bb1ec
SYCT <sict@bap.jp>
2024-09-26 11:35:48 +0700

    use flow

--- elixir/code/mycode/airports/lib/airports.ex
+++ elixir/code/mycode/airports/lib/airports.ex
@@ -17,9 +17,17 @@ defmodule Airports do
   def read_airport_use_flow(src, filter) do
     src.()
     |> File.stream!()
-    |> CSV.parse_stream()
     |> Flow.from_enumerable()
-    |> Flow.map(&row_binary_to_data/1)
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
     |> Flow.reject(filter)
     |> Enum.to_list()
   end
