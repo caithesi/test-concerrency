8eaf98ee8f9b4d4f9feb6630cd548693bf2025db
SYCT <sict@bap.jp>
2024-09-26 10:25:02 +0700

    change from list to stream

--- elixir/code/mycode/airports/lib/airports.ex
+++ elixir/code/mycode/airports/lib/airports.ex
@@ -5,23 +5,24 @@ defmodule Airport do
     Application.app_dir(:airports, "/priv/airports.csv")
   end
 
-  def read_airport_to_map(src) do
+  def read_airport_to_map(src, filter) do
     src.()
-    |> File.read!()
-    |> CSV.parse_string()
-    |> Enum.map(fn row ->
+    |> File.stream!()
+    |> CSV.parse_stream()
+    |> Stream.map(fn row ->
       %{
-        id: Enum.at(row, 0),
-        type: Enum.at(row, 2),
-        name: Enum.at(row, 3),
-        country: Enum.at(row, 8)
+        id: :binary.copy(Enum.at(row, 0)),
+        type: :binary.copy(Enum.at(row, 2)),
+        name: :binary.copy(Enum.at(row, 3)),
+        country: :binary.copy(Enum.at(row, 8))
       }
     end)
+    |> Stream.reject(filter)
   end
 
   def open_airports() do
     (&airports_csv/0)
-    |> read_airport_to_map()
-    |> Enum.reject(&(&1.type == "closed"))
+    |> read_airport_to_map(&(&1.type == "closed"))
+    |> Enum.to_list()
   end
 end
