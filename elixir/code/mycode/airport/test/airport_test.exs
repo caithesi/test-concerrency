1f7f0c1fb5aff30fee08d494ab03d5142e9247cc
SYCT <sict@bap.jp>
2024-09-26 10:17:48 +0700

    read csv

--- elixir/code/mycode/airport/test/airport_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AirportTest do
-  use ExUnit.Case
-  doctest Airport
-
-  test "greets the world" do
-    assert Airport.hello() == :world
-  end
-end
--- elixir/code/mycode/airports/lib/airports.ex
+++ elixir/code/mycode/airports/lib/airports.ex
@@ -1,5 +1,27 @@
 defmodule Airport do
+  alias NimbleCSV.RFC4180, as: CSV
+
   def airports_csv() do
     Application.app_dir(:airports, "/priv/airports.csv")
+  end
+
+  def read_airport_to_map(src) do
+    src.()
+    |> File.read!()
+    |> CSV.parse_string()
+    |> Enum.map(fn row ->
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+  end
+
+  def open_airports() do
+    (&airports_csv/0)
+    |> read_airport_to_map()
+    |> Enum.reject(&(&1.type == "closed"))
   end
 end
