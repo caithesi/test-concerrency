223274c6fd305472ac551774c2791a3f07734f02
SYCT <sict@bap.jp>
2024-10-01 17:08:40 +0700

    test on trigger

--- elixir/code/mycode/airports/lib/mini.ex
+++ elixir/code/mycode/airports/lib/mini.ex
@@ -1,21 +1,12 @@
-defmodule Airports do
+defmodule Mini do
   alias NimbleCSV.RFC4180, as: CSV
 
-  def airports_csv() do
-    Application.app_dir(:airports, "/priv/airports.csv")
-  end
-
-  def read_airport_use_stream(src, filter) do
-    src.()
-    |> File.stream!()
-    |> CSV.parse_stream()
-    |> Stream.map(&row_binary_to_data/1)
-    |> Stream.reject(filter)
-    |> Enum.to_list()
+  def mini_airports_csv() do
+    Application.app_dir(:airports, "/priv/mini.csv")
   end
 
   def read_airport_use_flow(src, filter) do
-    window = Flow.Window.trigger_every(Flow.Window.global(), 1000)
+    window = Flow.Window.trigger_every(Flow.Window.global(), 5)
 
     src.()
     |> File.stream!()
@@ -38,38 +29,27 @@ defmodule Airports do
     |> Flow.partition(window: window, key: {:key, :country})
     |> Flow.group_by(& &1.country)
     |> Flow.on_trigger(&trigger/3)
-    # |> Flow.reduce(fn -> %{} end, fn item, acc ->
-    #   Map.update(acc, item.country, 1, &(&1 + 1))
-    # end)
-    # |> Flow.take_sort(10, fn {_, a}, {_, b} -> a > b end)
-    # |> Flow.flat_map(fn x -> x end)
     |> Enum.to_list()
   end
 
   defp trigger(acc, _partition_info, {_type, _id, trigger}) do
     # Show progress in IEx, or use the data for something else.​
+    IO.puts("*****read acc******* [#{inspect(self())}] => #{inspect(acc)}  [********READ DONE ********]")
+    old_count = Map.split_with(acc, fn {key, _value} -> is_atom(key) end)
+    IO.puts("*****read count******* [#{inspect(self())}] #{inspect(old_count)}")
     event =
       acc
       |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
       |> IO.inspect(label: inspect(self()))
 
     case trigger do
-      :done -> {event, %{}}
-      {:every, 1000} -> {[], %{}}
+      :done -> {event, %{count: event}}
+      {:every, 5} -> {[], %{count: event}}
     end
   end
 
-  defp row_binary_to_data(row) do
-    %{
-      id: :binary.copy(Enum.at(row, 0)),
-      type: :binary.copy(Enum.at(row, 2)),
-      name: :binary.copy(Enum.at(row, 3)),
-      country: :binary.copy(Enum.at(row, 8))
-    }
-  end
-
   def open_airports() do
-    (&airports_csv/0)
+    (&mini_airports_csv/0)
     |> read_airport_use_flow(&(&1.type == "closed"))
   end
 end
