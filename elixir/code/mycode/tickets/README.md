2c8ff9fe6697c3a77929b3d7ca514f4dcb59cf37
SYCT <sict@bap.jp>
2024-10-03 11:26:04 +0700

    update readme, update dependecies, update code to connect to rabbit mq

--- elixir/code/mycode/tickets/README.md
+++ elixir/code/mycode/tickets/README.md
@@ -21,3 +21,5 @@ be found at <https://hexdocs.pm/tickets>.
 
 
 this project require rabbit mq to run
+
+docker run -d --hostname my-rabbit -p 15672:15672 -p 5672:5672 --name some-rabbit -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=password rabbitmq:3-management
--- elixir/code/mycode/tickets/lib/bookings_pipeline.ex
+++ elixir/code/mycode/tickets/lib/bookings_pipeline.ex
@@ -4,13 +4,19 @@ defmodule BookingsPipeline do
   @producer_config [
     queue: "bookings_queue",
     declare: [durable: true],
-    on_failure: :reject_and_requeue
+    on_failure: :reject_and_requeue,
+    connection: [
+      username: "user",
+      password: "password",
+      host: "localhost",
+      port: 5672
+    ]
   ]
   def start_link(_args) do
     options = [
       name: BookingsPipeline,
       producer: [
-        module: {@producer, @@producer_config}
+        module: {@producer, @producer_config}
       ],
       processors: [
         default: []
@@ -18,5 +24,9 @@ defmodule BookingsPipeline do
     ]
 
     Broadway.start_link(__MODULE__, options)
+  end
+
+  def handle_message(_processor, message, _context) do
+    IO.inspect(message, label: "Message")
   end
 end
